70cd58c0ecb7938aae35ead24f467614fac5ecc8
Leonewn <leon.ehrwein@students.hevs.ch>
2024-03-05 13:17:25 +0100

    add main class

---
+++ test/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int x = 10;
+        int y = 23;
+        System.out.println(x+y);
+    }
+}
